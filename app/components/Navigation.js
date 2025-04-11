2db5fcc1b5d49e9670b68972c3575d7df714ee03
carlcapili <carlcapili007@gmail.com>
2025-04-11 16:42:24 +0800

    hamburger menu / reponse in md

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,28 +1,35 @@
+'use client'
 import localfont from 'next/font/local'
 import Link from 'next/link'
 import { BsGithub } from 'react-icons/bs'
 import { BsFacebook } from 'react-icons/bs'
 import { BsLinkedin } from 'react-icons/bs'
+import Hamburger from 'hamburger-react'
+import { useState } from 'react'
 
 export default function Navigation() {
+  const [isOpen, setOpen] = useState(false)
   return (
     <div>
       <nav className='flex justify-between items-center h-24 w-full'>
         <div className='flex items-center'>
-          <div className='px-[8vw]'>
+          <div className='px-[8vw] md:flex'>
             <span className='text-2xl'>CJ</span>
           </div>
-          <ul className='items-center gap-[3vw] text-1xl py-8 px-[1vw] hidden md:flex'>
+          <ul className='hidden md:flex items-center gap-[3vw] text-1xl py-8 px-[1vw]'>
             <li className='hover:text-gray-700 mx-2'> About </li>
             <li className='hover:text-gray-700 mx-2'> Projects </li>
             <li className='hover:text-gray-700 mx-2'> Technologies </li>
             <li className='hover:text-gray-700 mx-2'> Reach Me Out </li>
           </ul>
         </div>
-        <div className='md:flex hidden gap-[1.5vw] items-center y-6 pr-[15%]'>
+        <div className='hidden md:flex gap-[1.5vw] items-center y-6 pr-[15%]'>
           <BsGithub size={24} />
           <BsFacebook size={24} />
           <BsLinkedin size={24} />
+        </div>
+        <div className='md:hidden px-17'>
+          <Hamburger toggled={isOpen} toggle={setOpen} size={28} />
         </div>
       </nav>
     </div>
