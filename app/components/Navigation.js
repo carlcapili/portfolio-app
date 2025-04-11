e9fdc6e23ad9763b1ca941201e325088227277b6
carlcapili <carlcapili007@gmail.com>
2025-04-11 15:56:29 +0800

    responsive ( no hamburger menu )

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -10,16 +10,16 @@ export default function Navigation() {
       <nav className='flex justify-between items-center h-24 w-full'>
         <div className='flex items-center'>
           <div className='px-[8vw]'>
-            <span className='text-2xl'>Logo</span>
+            <span className='text-2xl'>CJ</span>
           </div>
-          <ul className='flex items-center gap-[3vw] text-1xl py-8 px-[1vw]'>
-            <li className='hover:text-gray-700'> About </li>
-            <li className='hover:text-gray-700'> Projects </li>
-            <li className='hover:text-gray-700'> Technologies </li>
-            <li className='hover:text-gray-700'> Reach Me Out </li>
+          <ul className='items-center gap-[3vw] text-1xl py-8 px-[1vw] hidden md:flex'>
+            <li className='hover:text-gray-700 mx-2'> About </li>
+            <li className='hover:text-gray-700 mx-2'> Projects </li>
+            <li className='hover:text-gray-700 mx-2'> Technologies </li>
+            <li className='hover:text-gray-700 mx-2'> Reach Me Out </li>
           </ul>
         </div>
-        <div className='flex gap-[1.5vw] items-center y-6 pr-[15%]'>
+        <div className='md:flex hidden gap-[1.5vw] items-center y-6 pr-[15%]'>
           <BsGithub size={24} />
           <BsFacebook size={24} />
           <BsLinkedin size={24} />
