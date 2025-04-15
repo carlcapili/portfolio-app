f26cf68640167252de218be0205967aca4e102fb
carlcapili <carlcapili007@gmail.com>
2025-04-15 16:47:53 +0800

    hero section 2

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,21 +1,15 @@
 import Image from 'next/image'
 export default function Hero() {
   return (
-    <div className='container mx-auto items-left translate-y-3  h-screen'>
-      <div className='flex space-between py-25'>
-        <h2 className='text-5xl mx-auto h-[500px] w-[800px] p-1'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
-          perferendis!
-        </h2>
-        <div className='flex mx-auto justify-center'>
-          <Image
-            src='/images/technoblade.webp'
-            alt='technoblade'
-            width={400}
-            height={500}
-          />
+    <div className='flex justify-center gap-[30vw] items-center container mx-auto items-left translate-y-1 h-screen bg-slate-300'>
+      <div className='-translate-x-25 -translate-y-20'>
+        <div>hi im cj </div>
+        <div className='w-[300px] my-3'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, sunt
+          vel! Consectetur suscipit sed omnis totam beatae possimus. At, fugiat.
         </div>
       </div>
+      <div>image here</div>
     </div>
   )
 }
